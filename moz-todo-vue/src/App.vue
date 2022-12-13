89f4187f0723c1d6a6dc9275f4388aba9cb565e5
kzh4295 <kzh4295@ncits.co.kr>
2022-12-13 17:14:05 +0900

    TIL 생성

---
+++ moz-todo-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>To-DO List</h1>
+    <todo-item label="jh"></todo-item>
+  </div>
+</template>
+
+<script>
+import ToDoItem from './components/TodoItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'todo-item':ToDoItem,
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>

eda2e350212c94225dd69d92f86076a4a48022f8
kzh4295 <kzh4295@ncits.co.kr>
2023-02-14 16:37:43 +0900

    세팅

---
+++ crud/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="left">
+    <input placeholder="Filter prefix"/>
+    <textarea></textarea>
+    <button>Create</button>
+    <button>Update</button>
+    <button>Delete</button>
+  </div>
+  <div class="right">
+    <input />
+    <input />
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.left{
+
+}
+.right{
+
+}
+</style>
